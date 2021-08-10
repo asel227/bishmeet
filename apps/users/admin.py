3bf93e77320566fab41f23d57805434f9a137dac
unknown <melisovaasel7@gmail.com>
2021-08-10 22:11:13 +0600

    Create useradmin

---
+++ apps/users/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from apps.users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'last_name', 'first_name', 'age', 'gender', 'email', 'get_avatar',
+    )
+
+    def get_avatar(self, obj):
+        if obj.avatar:
+            return mark_safe(
+                f'<img src={obj.avatar.url} width="100" />'
+            )
+
+        return '-'
+
+    get_avatar.short_description = 'Фото'
---
+++ apps/users/services.py
@@ -0,0 +1,39 @@
+# from django.contrib.auth import get_user_model
+#
+# import re
+#
+#
+# User = get_user_model()
+#
+#
+# def authenticate(email, password):
+#     try:
+#         user = User.objects.get(email=email)
+#     except User.DoesNotExist:
+#         return None
+#     else:
+#         if user.check_password(password):
+#             return user
+#     return None
+#
+#
+# def create_user(data: dict):
+#     user = User(email=data.get('email'))
+#
+#     user.set_password(data.get('pass1'))
+#     user.first_name = data.get('first_name')
+#     user.last_name = data.get('last_name')
+#     user.email = data.get('email')
+#
+#     user.save()
+#
+#     return user
+#
+#
+# phone_number_regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+#
+#
+# def check_email(email):
+#     if re.search(phone_number_regex, email):
+#         return True
+#     return False
